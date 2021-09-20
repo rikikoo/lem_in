ee4dbd3bdc6da6e05faab1f8e8fbb9cf23947d4d
Riki Kyttälä <rkyttala@student.hive.fi>
2021-09-21 01:04:02 +0300

    added move verifier script

--- verify.py
+++ verify.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 import sys
 
 if len(sys.argv) < 2:
-    print(f"Usage: {sys.argv[0]} <lem-in_output_file>")
+    print(f"Usage: [python3] {sys.argv[0]} <lem-in_output_file>")
     quit()
 
 filepath = sys.argv[1]
@@ -12,20 +14,37 @@ try:
         lem_out = []
         for line in f:
             lem_out.append(line)
-except:
-    print(f"Usage: {sys.argv[0]} <lem-in_output_file>")
-    quit()
+except Exception:
+    print(f"Error: {Exception}\nUsage: [python3] {sys.argv[0]} <lem-in_output_file>")
+    raise
 
 # store total ants, sink, and moves
 ants = int(lem_out[0])
-
+move_list = []
+sink = None
 for i, line in enumerate(lem_out):
-    if line == "##end":
-        sink = lem_out[i + 1]
+    if "##end" in line:
+        sink = lem_out[i + 1].split()[0]
         continue
     if line[0] == 'L':
-        moves = lem_out[i:]
+        move_list = lem_out[i:]
         break
 
-for turn in moves:
-    for 
+for turn in move_list:
+    moves = turn.strip().split(' ')
+    ants = []
+    rooms = []
+    for move in moves:
+        ants.append(move.split('-')[0][1:])
+        rooms.append(move.split('-')[1])
+    if len(ants) != len(list(set(ants))):
+        for i, ant in enumerate(ants):
+            if ant in ants[i + 1:]:
+                print(f"Same ant moved twice on a single turn ({ant})!\n{turn}\n")
+                break
+    if sink not in rooms and len(rooms) != len(list(set(rooms))):
+        for i, room in enumerate(rooms):
+            if room in rooms[i + 1:]:
+                print(f"Multiple ants in same room ({room})!\n{turn}\n")
+                break
+
